d6b0de655f11d9cd376a837f43d811b8d5c75f2a
David Cox <dcox@bmc.com>
2017-05-12 12:48:28 -0500

    First iteration of the real plugin

--- init.lua
+++ init.lua
@@ -1,42 +1,56 @@
-local framework = require('framework.lua')
-local Plugin = framework.Plugin
-local MeterDataSource = framework.MeterDataSource
-local each = framework.functional.each
-local pack = framework.util.pack
+local io = require('io')
+local json = require('json')
 
-local params = framework.params
+--
+-- Read and parse the plugin file.
+--
+plugin_file = io.open('plugin.json', 'r')
+plugin_json = plugin_file:read("*all")
+plugin_info = json.parse(plugin_json)
+plugin_file:close()
 
--- Create a new MeterDataSource which communicates
--- to the meter via its RPC channel using a non-closing connection.
-local data_source = MeterDataSource:new()
+--
+--  Read the parameter file.  It will contain an array of items which define
+--  the counter metrics to collect.
+--
+param_file = io.open('param.json', 'r')
+params_json = param_file:read("*all")
+params = json.parse(params_json)
+param_file:close()
 
--- Callback to fetch the CPU percent usage for each
--- CPU on the target system
-function data_source:onFetch(socket)
-  socket:write(self:queryMetricCommand({match = 'system.cpu.usage'}))
+--
+-- Loop through each metric and store the parameters.  Currently supported are
+-- counter= The counter path
+-- factor=  The multiplication factor for the counter value.
+--
+
+local metrics = {}
+for _,item in pairs(params.items) do
+	metrics[item.metric] = {counter = item.counter, factor = item.factor}
 end
 
--- Allocate a meter plugin instance that handles polling
--- for measurements
-local plugin = Plugin:new(params, data_source)
+--
+-- Create the .ini file that the meter will parse.
+-- The filename needs to be [plugin_name]_pdh.ini
+--
+--
+file = io.open("win_pdh_test_pdh.ini", "w")
 
--- Called once each time the framework requests 
--- measurements be collected. This callback is required to return
--- a table of measurement strings which consist of the following:
--- CPU_CORE <cpu usage in percent> <measurement timestamp> <cpu name>
-function plugin:onParseValues(data)
-  local result = {}
- 
-  -- Iterate over CPU usage data returned from the meter and
-  -- add to the result table
-  each(function (v, i) 
-    local metric, cpu_id = v.metric:match('^(system%.cpu%.usage)|cpu=(%d+)$')
-    if metric then
-      table.insert(result, pack('CPU_CORE', v.value, v.timestamp, plugin.source .. '-C' .. cpu_id))
-    end
-  end, data)
-
-  return result
+if (params.pollInterval ~= nil) then
+    file:write("interval_sec=" .. params.pollInterval .. "\n")
 end
 
-plugin:run()
+for k, v in pairs(metrics) do
+	file:write("[".. k .. "]\n")
+	file:write("  counter=".. v.counter.."\n")
+	if (v.factor ~= nil) then
+	    file:write("  factor=".. v.factor.."\n")
+	end
+end
+file:close()
+
+--
+-- Send our startup event
+--
+
+print ("_bevent:"..plugin_info.name.." version " .. plugin_info.version .. " Started|t:info")
